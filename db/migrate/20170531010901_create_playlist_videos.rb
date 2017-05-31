a3a8e782d0f1de2b7523eabbc1955b6f3b39428b
Idayat <github email address>
2017-05-30 21:40:29 -0400

    setting up schema

--- db/migrate/20170531010901_create_playlist_videos.rb
+++ db/migrate/20170531010901_create_playlist_videos.rb
@@ -2,6 +2,11 @@ class CreatePlaylistVideos < ActiveRecord::Migration[5.0]
   def change
     create_table :playlist_videos do |t|
 
+      t.references :video, foreign_key: true
+      t.references :playlist, foreign_key: true
+      t.string :name
+      t.string :videoId
+
       t.timestamps
     end
   end
