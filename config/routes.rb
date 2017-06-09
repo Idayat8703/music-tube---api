5d3951c97a9a5e6ebb976775fbdba58cff2093d4
Idayat <github email address>
2017-06-08 21:12:18 -0400

    fix readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+
+      #/api/v1/users
+      resources :users, only: [:create]
+      #/api/v1/playlists
+      resources :playlists do
+        resources :videos, only: [:create, :index, :destroy]
+      end
+      #api/v1/auth
+      post '/auth', to: "auth#login"
+      post '/auth/refresh', to: "auth#refresh"
+
+    end
+  end
 end
